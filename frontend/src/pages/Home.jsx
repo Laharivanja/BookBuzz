09ce41f51ca06719d0dd86f6e6a0e7fecdd78630
GeethikaMaddi <mgeethika490@gmail.com>
2025-08-25 10:35:59 +0530

    Set up Vite env config for API base URL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://bookbuzz-wzqp.onrender.com/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
